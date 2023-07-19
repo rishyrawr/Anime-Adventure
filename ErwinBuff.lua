d563c68d83f7e356dd03230aecdad095980af2a7
rishyrawr <139991713+rishyrawr@users.noreply.github.com>
2023-07-19 23:31:35 +0700

    Create ErwinBuff.lua

---
+++ ErwinBuff.lua
@@ -0,0 +1,26 @@
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name)
+repeat task.wait() until game:GetService("Workspace")["_waves_started"].Value == true
+
+while wait() do
+local erwinbuff = {}
+
+for _,v in pairs(game:GetService("Workspace")._UNITS:GetChildren()) do
+    if v.Name == 'erwin' and v._stats.player.Value == game.Players.LocalPlayer then
+        table.insert(erwinbuff, v)
+    end
+end
+
+if #erwinbuff == 4 then
+    while true do
+        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(erwinbuff[1])
+        wait(15.4)
+        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(erwinbuff[3])
+        wait(15.4)
+        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(erwinbuff[2])
+        wait(15.4)
+        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(erwinbuff[4])
+        wait(15.4)
+    end
+end
+end
