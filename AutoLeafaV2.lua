47f60e64329124c5e4424cb67e560b0b2d716d37
Rishy <139991713+rishyrawr@users.noreply.github.com>
2023-07-24 15:44:28 +0700

    Update AutoLeafaV2.lua

--- AutoLeafaV2.lua
+++ AutoLeafaV2.lua
@@ -1,28 +1,24 @@
-function Auto_Leafy(v)
-    s = v
-    if v then
-        local LeafyCount = 0
-        local maxLeafyCount = 4
-        local LeafyTable = {}
-        while LeafyCount ~= maxLeafyCount do
-        
-        for i, unit in pairs(_UNITS:GetChildren()) do
-            if unit:FindFirstChild('_stats') and unit:FindFirstChild('_stats').id.Value == "leafa" and unit:WaitForChild('_stats').player.Value == game.Players.LocalPlayer then
-                LeafyCount = LeafyCount + 1
-                table.insert(LeafyTable, unit)
-            end
-        end
-        if #LeafyTable ~= maxLeafyCount then
-            task.wait(5)
-        end
-            while LeafyCount == maxLeafyCount do
-                for i = 1, LeafyCount do
-                    use_active_attack(LeafyTable[i])
-                    task.wait(16.5)
-                end
-            end
+repeat task.wait() until game:IsLoaded() == true
+
+while task.wait() do
+    local LeafaTable = {}
+    local Active_Attack = game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack
+
+    for _,v in pairs(game:GetService("Workspace")._UNITS:GetChildren()) do
+        if v.Name == 'leafa' and v:WaitForChild('_stats').player.Value == game.Players.LocalPlayer then
+            table.insert(LeafaTable, v)
         end
     end
-end 
 
-Auto_Leafy(v)
+    if #LeafaTable = 4 then
+        while true do
+            Active_Attack:InvokeServer(LeafaTable[1])
+            task.wait(16.5)
+            Active_Attack:InvokeServer(LeafaTable[2])
+            task.wait(16.5)
+            Active_Attack:InvokeServer(LeafaTable[3])
+            task.wait(16.5)
+            Active_Attack:InvokeServer(LeafaTable[4])
+        end
+    end
+end
