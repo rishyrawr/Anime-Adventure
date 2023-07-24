ebfc08429f1fd4d22a77bc33adc307465d0c8639
Rishy <139991713+rishyrawr@users.noreply.github.com>
2023-07-24 17:49:57 +0700

    Update LeafaBuff.lua

--- LeafaBuff.lua
+++ LeafaBuff.lua
@@ -1,6 +1,6 @@
 repeat task.wait() until game:IsLoaded() == true
-repeat task.wait() until game:GetService('Workspace'):WaitForChild('_waves_started').Value == true
-repeat task.wait() until game:GetService('Workspace'):FindFirstChild('_UNITS') ~= nil
+repeat task.wait() until game:GetService("Workspace"):WaitForChild("_waves_started").Value == true
+repeat task.wait() until game:GetService("Workspace"):FindFirstChild("_UNITS") ~= nil
 
 local LeafyCount = 0
 local use_active = game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack
@@ -8,8 +8,8 @@ local use_active = game:GetService("ReplicatedStorage").endpoints["client_to_ser
 while LeafyCount ~= 4 do task.wait()
     local LeafyTable = {}
 
-    for _, unit in pairs(game:GetService('Workspace')._UNITS:GetChildren()) do
-        if unit.Name == 'leafa' and unit:FindFirstChild('_stats').id.Value == "leafa" and unit:WaitForChild('_stats').player.Value == game.Players.LocalPlayer then
+    for _, unit in pairs(game:GetService("Workspace")._UNITS:GetChildren()) do
+        if unit.Name == "leafa" and unit:FindFirstChild("_stats").id.Value == "leafa" and unit:WaitForChild("_stats").player.Value == game.Players.LocalPlayer then
             LeafyCount = LeafyCount + 1
             table.insert(LeafyTable, unit)
         end
