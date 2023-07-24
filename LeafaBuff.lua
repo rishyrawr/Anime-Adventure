ddaba035e7ba149bf12a081d7820c1fa4c9eadce
Rishy <139991713+rishyrawr@users.noreply.github.com>
2023-07-24 15:43:53 +0700

    Update LeafaBuff.lua

--- LeafaBuff.lua
+++ LeafaBuff.lua
@@ -1,24 +1,26 @@
 repeat task.wait() until game:IsLoaded() == true
+repeat task.wait() until game:GetService('Workspace'):WaitForChild('_waves_started').Value == true
+repeat task.wait() until game:GetService('Workspace'):FindFirstChild('_UNITS') ~= nil
 
-while task.wait() do
-    local LeafaTable = {}
-    local Active_Attack = game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack
+while task.wait() do wait()
+local leafabuff = {}
 
-    for _,v in pairs(game:GetService("Workspace")._UNITS:GetChildren()) do
-        if v.Name == 'leafa' and v:WaitForChild('_stats').player.Value == game.Players.LocalPlayer then
-            table.insert(LeafaTable, v)
-        end
+for _,v in pairs(game:GetService("Workspace")._UNITS:GetChildren()) do
+    if v.Name == 'leafa' and v:WaitForChild('_stats').player.Value == game.Players.LocalPlayer then
+        table.insert(leafabuff, v)
     end
+end
 
-    if #LeafaTable = 4 then
-        while true do
-            Active_Attack:InvokeServer(LeafaTable[1])
-            task.wait(16.5)
-            Active_Attack:InvokeServer(LeafaTable[2])
-            task.wait(16.5)
-            Active_Attack:InvokeServer(LeafaTable[3])
-            task.wait(16.5)
-            Active_Attack:InvokeServer(LeafaTable[4])
-        end
+if #leafabuff == 4 then
+    while true do
+        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(leafabuff[1])
+        wait(16.4)
+        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(leafabuff[3])
+        wait(16.4)
+        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(leafabuff[2])
+        wait(16.4)
+        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(leafabuff[4])
+        wait(16.4)
     end
+end
 end
