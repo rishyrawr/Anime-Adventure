b2bd54bb85b2555b00fbbf1c7bd392f9a8199f30
Rishy <139991713+rishyrawr@users.noreply.github.com>
2023-07-24 14:26:30 +0700

    Update LeafaBuff.lua

--- LeafaBuff.lua
+++ LeafaBuff.lua
@@ -14,13 +14,13 @@ end
 if #leafabuff == 4 then
     while true do
         game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(leafabuff[1])
-        wait(15.4)
+        wait(16.4)
         game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(leafabuff[3])
-        wait(15.4)
+        wait(16.4)
         game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(leafabuff[2])
-        wait(15.4)
+        wait(16.4)
         game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(leafabuff[4])
-        wait(15.4)
+        wait(16.4)
     end
 end
 end
