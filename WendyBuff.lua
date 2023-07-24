044a4e6b97e9f7afd5753524fdfd67e9da2384fb
Rishy <139991713+rishyrawr@users.noreply.github.com>
2023-07-24 20:26:33 +0700

    Update WendyBuff.lua

--- WendyBuff.lua
+++ WendyBuff.lua
@@ -1,21 +1,20 @@
--- NOTE: NOT WORK W/ XENON HUB
 repeat task.wait() until game:IsLoaded() == true
 repeat task.wait() until game:GetService('Workspace'):WaitForChild('_waves_started').Value == true
 repeat task.wait() until game:GetService('Workspace'):FindFirstChild('_UNITS') ~= nil
-
+    
 while task.wait() do
-    local wendybuff = {}
-
-    for _,v in pairs(game:GetService("Workspace")._UNITS:GetChildren()) do
+    local Wendybuff = {}
+    
+    for _,v in pairs(game:GetService('Workspace')._UNITS:GetChildren()) do
         if v.Name == 'wendy' and v:WaitForChild('_stats').player.Value == game.Players.LocalPlayer then
-            table.insert(wendybuff, v)
+            table.insert(Wendybuff, v)
         end
     end
-
-    if #wendybuff == 4 then
+    
+    if #Wendybuff == 4 then
         while true do
             for i = 1, 4 do
-                game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(wendybuff[i])
+                game:GetService('ReplicatedStorag').endpoints['client_to_server'].use_active_attack:InvokeServer(Wendybuff[i])
                 task.wait(15.5)
             end
         end
