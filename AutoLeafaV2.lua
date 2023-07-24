0ad2717b020c546a32f2a2947e6cfc0db04b1b8a
Rishy <139991713+rishyrawr@users.noreply.github.com>
2023-07-24 15:00:18 +0700

    Update AutoLeafaV2.lua

--- AutoLeafaV2.lua
+++ AutoLeafaV2.lua
@@ -7,7 +7,7 @@ function Auto_Leafy(v)
         while LeafyCount ~= maxLeafyCount do
         
         for i, unit in pairs(_UNITS:GetChildren()) do
-            if unit:FindFirstChild('_stats') and unit:FindFirstChild('_stats').id.Value == "leafa" then
+            if unit:FindFirstChild('_stats') and unit:FindFirstChild('_stats').id.Value == "leafa" and unit:WaitForChild('_stats').player.Value == game.Players.LocalPlayer then
                 LeafyCount = LeafyCount + 1
                 table.insert(LeafyTable, unit)
             end
