9f21c13ec610d6b7c57c81f14903400892833f6b
Rishy <139991713+rishyrawr@users.noreply.github.com>
2023-07-24 20:08:26 +0700

    Update WendyBuff.lua

--- WendyBuff.lua
+++ WendyBuff.lua
@@ -5,23 +5,21 @@ repeat task.wait() until game:GetService("Workspace"):FindFirstChild("_UNITS") ~
 
 local use_active = game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack
 
-while true do task.wait()
+while task.wait() do
     local WendaTable = {}
     for _, unit in pairs(game:GetService("Workspace")._UNITS:GetChildren()) do
-        if unit.Name == "wendy" and unit:FindFirstChild("_stats").id.Value == "wendy" and unit:WaitForChild("_stats").player.Value == game.Players.LocalPlayer then
+        if unit.Name == "wendy" and unit:WaitForChild("_stats").player.Value == game.Players.LocalPlayer then
             table.insert(WendaTable, unit)
         end
     end
     
     if #WendaTable == 4 then
-        while true do
-            use_active:InvokeServer(WendaTable[1])
-            task.wait(15.4)
-            use_active:InvokeServer(WendaTable[2])
-            task.wait(15.4)
-            use_active:InvokeServer(WendaTable[3])
-            task.wait(15.4)
-            use_active:InvokeServer(WendaTable[4])
-        end
+        use_active:InvokeServer(WendaTable[1])
+        wait(15.4)
+        use_active:InvokeServer(WendaTable[2])
+        wait(15.4)
+        use_active:InvokeServer(WendaTable[3])
+        wait(15.4)
+        use_active:InvokeServer(WendaTable[4])
     end
 end
