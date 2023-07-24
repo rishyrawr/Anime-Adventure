3141bd8a4ad6e25c62e21721346bb22b7f81e7df
Rishy <139991713+rishyrawr@users.noreply.github.com>
2023-07-24 16:44:23 +0700

    Update LeafaBuff.lua

--- LeafaBuff.lua
+++ LeafaBuff.lua
@@ -2,25 +2,23 @@ repeat task.wait() until game:IsLoaded() == true
 repeat task.wait() until game:GetService('Workspace'):WaitForChild('_waves_started').Value == true
 repeat task.wait() until game:GetService('Workspace'):FindFirstChild('_UNITS') ~= nil
 
-while task.wait() do wait()
-local leafabuff = {}
+local LeafyCount = 0
 
-for _,v in pairs(game:GetService("Workspace")._UNITS:GetChildren()) do
-    if v.Name == 'leafa' and v:WaitForChild('_stats').player.Value == game.Players.LocalPlayer then
-        table.insert(leafabuff, v)
-    end
-end
+while LeafyCount ~= 4 do task.wait()
+    local use_active = game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack
+    local LeafyTable = {}
 
-if #leafabuff == 4 then
-    while true do
-        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(leafabuff[1])
-        wait(16.4)
-        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(leafabuff[3])
-        wait(16.4)
-        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(leafabuff[2])
-        wait(16.4)
-        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(leafabuff[4])
-        wait(16.4)
+    for _, unit in pairs(game:GetService('Workspace')._UNITS:GetChildren()) do
+        if unit.Name == 'leafa' and unit:WaitForChild('_stats').player.Value == game.Players.LocalPlayer then
+            LeafyCount = LeafyCount + 1
+            table.insert(LeafyTable, unit)
+        end
     end
-end
+
+    while LeafyCount == 4 do
+        for i = 1, LeafyCount do
+            use_active:InvokeServer(LeafyTable[i])
+            task.wait(16.4)
+        end
+    end
 end
