31740adddbf029e7688b2d75b6f804740b0b5822
Rishy <139991713+rishyrawr@users.noreply.github.com>
2023-07-24 20:28:31 +0700

    Update LeafaBuff.lua

--- LeafaBuff.lua
+++ LeafaBuff.lua
@@ -1,27 +1,22 @@
--- NOTE: NOT WORK W/ XENON HUB
 repeat task.wait() until game:IsLoaded() == true
-repeat task.wait() until game:GetService("Workspace"):WaitForChild("_waves_started").Value == true
-repeat task.wait() until game:GetService("Workspace"):FindFirstChild("_UNITS") ~= nil
-
-local use_active = game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack
-
-while true do task.wait()
-    local LeafaTable = {}
-    for _, unit in pairs(game:GetService("Workspace")._UNITS:GetChildren()) do
-        if unit.Name == "leafa" and unit:WaitForChild("_stats").player.Value == game.Players.LocalPlayer then
-            table.insert(LeafaTable, unit)
+repeat task.wait() until game:GetService('Workspace'):WaitForChild('_waves_started').Value == true
+repeat task.wait() until game:GetService('Workspace'):FindFirstChild('_UNITS') ~= nil
+    
+while task.wait() do
+    local leafabuff = {}
+    
+    for _,v in pairs(game:GetService('Workspace')._UNITS:GetChildren()) do
+        if v.Name == 'leafa' and v:WaitForChild('_stats').player.Value == game.Players.LocalPlayer then
+            table.insert(leafabuff, v)
         end
     end
     
-    if #LeafaTable == 4 then
+    if #leafabuff == 4 then
         while true do
-            use_active:InvokeServer(LeafaTable[1])
-            task.wait(15.4)
-            use_active:InvokeServer(LeafaTable[2])
-            task.wait(15.4)
-            use_active:InvokeServer(LeafaTable[3])
-            task.wait(15.4)
-            use_active:InvokeServer(LeafaTable[4])
+            for i = 1, 4 do
+                game:GetService('ReplicatedStorage').endpoints['client_to_server'].use_active_attack:InvokeServer(leafabuff[i])
+                task.wait(15.5)
+            end
         end
     end
 end
