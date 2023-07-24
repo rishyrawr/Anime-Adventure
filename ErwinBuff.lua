cec2ae3d91081350c27ff7c4948773e0a2a41372
Rishy <139991713+rishyrawr@users.noreply.github.com>
2023-07-24 17:49:02 +0700

    Update ErwinBuff.lua

--- ErwinBuff.lua
+++ ErwinBuff.lua
@@ -8,15 +8,15 @@ while ErwinCount ~= 4 do task.wait()
     local use_active = game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack
     local ErwinTable = {}
 
-    for _, unit in pairs(game:GetService('Workspace')._UNITS:GetChildren()) do
-        if unit.Name == 'erwin' and unit:WaitForChild('_stats').player.Value == game.Players.LocalPlayer then
+    for _, unit in pairs(game:GetService("Workspace")._UNITS:GetChildren()) do
+        if unit.Name == "erwin" and unit:FindFirstChild("_stats").id.Value == "erwin" and unit:WaitForChild("_stats").player.Value == game.Players.LocalPlayer then
             ErwinCount = ErwinCount + 1
             table.insert(ErwinTable, unit)
         end
     end
 
     while ErwinCount == 4 do
-        for i = 1,ErwinCount do
+        for i = 1, ErwinCount do
             use_active:InvokeServer(ErwinTable[i])
             task.wait(15.4)
         end
