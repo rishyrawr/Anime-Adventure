fc23161287c8c5599722858d4f9bc5bfb2bd9d9c
Rishy <139991713+rishyrawr@users.noreply.github.com>
2023-07-24 17:46:54 +0700

    Update WendyBuff.lua

--- WendyBuff.lua
+++ WendyBuff.lua
@@ -3,13 +3,13 @@ repeat task.wait() until game:GetService('Workspace'):WaitForChild('_waves_start
 repeat task.wait() until game:GetService('Workspace'):FindFirstChild('_UNITS') ~= nil
 
 local WendaCount = 0
+local use_active = game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack
 
 while WendaCount ~= 4 do task.wait()
-    local use_active = game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack
     local WendaTable = {}
 
     for _, unit in pairs(game:GetService('Workspace')._UNITS:GetChildren()) do
-        if unit.Name == 'wenda' and unit:WaitForChild('_stats').player.Value == game.Players.LocalPlayer then
+        if unit.Name == 'wendy' and unit:FindFirstChild('_stats').id.Value == "wendy" and unit:WaitForChild('_stats').player.Value == game.Players.LocalPlayer then
             WendaCount = WendaCount + 1
             table.insert(WendaTable, unit)
         end
