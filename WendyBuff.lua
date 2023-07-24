d7ca18d338b33e7688c5e0e5bf47c3fa963e5d76
Rishy <139991713+rishyrawr@users.noreply.github.com>
2023-07-24 20:24:05 +0700

    Update WendyBuff.lua

--- WendyBuff.lua
+++ WendyBuff.lua
@@ -4,24 +4,20 @@ repeat task.wait() until game:GetService('Workspace'):WaitForChild('_waves_start
 repeat task.wait() until game:GetService('Workspace'):FindFirstChild('_UNITS') ~= nil
 
 while task.wait() do
-local wendybuff = {}
+    local wendybuff = {}
 
-for _,v in pairs(game:GetService("Workspace")._UNITS:GetChildren()) do
-    if v.Name == 'wendy' and v:WaitForChild('_stats').player.Value == game.Players.LocalPlayer then
-        table.insert(wendybuff, v)
+    for _,v in pairs(game:GetService("Workspace")._UNITS:GetChildren()) do
+        if v.Name == 'wendy' and v:WaitForChild('_stats').player.Value == game.Players.LocalPlayer then
+            table.insert(wendybuff, v)
+        end
     end
-end
 
-if #wendybuff == 4 then
-    while true do
-        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(wendybuff[1])
-        wait(15.5)
-        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(wendybuff[3])
-        wait(15.5)
-        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(wendybuff[2])
-        wait(15.5)
-        game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(wendybuff[4])
-        wait(15.5)
+    if #wendybuff == 4 then
+        while true do
+            for i = 1, 4 do
+                game:GetService("ReplicatedStorage").endpoints["client_to_server"].use_active_attack:InvokeServer(wendybuff[i])
+                task.wait(15.5)
+            end
+        end
     end
-end
 end
