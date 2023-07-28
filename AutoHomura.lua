ee0614751c0fc9a39a11c3be5bc80c2919ffa98f
Rishy <139991713+rishyrawr@users.noreply.github.com>
2023-07-28 16:02:49 +0700

    Update AutoHomura.lua

--- AutoHomura.lua
+++ AutoHomura.lua
@@ -1,24 +1,11 @@
--- CREDS TO SOMEONE IN TRAP HUB SERVER
-getgenv().AutoHomura = true
-
-coroutine.resume(coroutine.create(function()
-    while task.wait() do 
-        if AutoHomura and game.PlaceId == 8349889591 then
-            local success,err = pcall(function()
-                repeat task.wait() until Workspace:WaitForChild("_UNITS")
-                local plyr = game.Players.LocalPlayer
-                for i, v in ipairs(Workspace["_UNITS"]:GetChildren()) do
-                    if v:FindFirstChild("_stats") then
-                        if v._stats.id.Value == "homura_evolved" and v._stats.player.Value == plyr and v._stats.state.Value == 'attack' then
-                            game:GetService("ReplicatedStorage").endpoints.client_to_server.use_active_attack:InvokeServer(v)
-                        end
-                    end
-                end 
-            end)
-
-            if err then
-                -- print(err)
-            end
+repeat task.wait() until game:IsLoaded() == true
+repeat task.wait() until game:GetService('Workspace'):WaitForChild('_waves_started').Value == true
+repeat task.wait() until game:GetService('Workspace'):FindFirstChild('_UNITS') ~= nil
+    
+while task.wait() do
+    for _,v in pairs(game:GetService('Workspace')._UNITS:GetChildren()) do
+        if v._stats.id.Value == "homura_evolved" and v._stats.player.Value == game.Players.LocalPlayer.Name and v._stats.state.Value == 'attack' then
+            game:GetService("ReplicatedStorage").endpoints.client_to_server.use_active_attack:InvokeServer(v)
         end
     end
-end))
+end
